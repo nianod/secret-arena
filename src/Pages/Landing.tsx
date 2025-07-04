4b69a779c394705f75bb9219fa3ad247bf146e20
nianod <arnoldkk422@gmail.com>
2025-07-04 23:29:39 +0300

    still ui

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -4,8 +4,12 @@ const Landing = () => {
     <div>
       <h1 className="text-red-400 font-bold text-5xl">WELCOME TO VIBLY ANONYMOUS</h1>
       <div className="text-white flex">
-        <h2>That secret you've being Hiding😂</h2>
-        <img src="Screenshot 2025-07-04 215927.png" alt="secret" />
+        <div className="w-[50%]">
+          <h2 >That secret you've being Hiding😂</h2>
+        </div>
+        <div>
+          <img src="Screenshot 2025-07-04 215927.png" alt="secret" />
+        </div>
       </div>
     </div>
   )
