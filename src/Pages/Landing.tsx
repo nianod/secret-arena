ed3a80589cc00650880194d52512922ece62be84
nianod <arnoldkk422@gmail.com>
2025-07-05 13:35:43 +0300

    landing

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -2,10 +2,10 @@
 const Landing = () => {
   return (
     <div>
-      <h1 className="text-red-400 font-bold text-5xl">WELCOME TO VIBLY ANONYMOUS</h1>
-      <div className="text-white flex">
+      <h1 className="text-red-400 font-bold text-5xl text-center">WELCOME TO VIBLY ANONYMOUS</h1>
+      <div className="text-white flex mt-50 p-10">
         <div className="w-[50%]">
-          <h2 >That secret you've being Hiding😂</h2>
+          <h2 className="text-3xl font-black font-[helvetica] underline text-red-500">That secret you've being Hiding</h2>
         </div>
         <div>
           <img src="Screenshot 2025-07-04 215927.png" alt="secret" />
