3faa4fec4f6c6a548d17c546d431a130ea2de759
nianod <arnoldkk422@gmail.com>
2025-07-05 14:19:52 +0300

    landing

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,10 +1,13 @@
 
 const Landing = () => {
 
-  const definitions: { speakUp: string; identity: string; imag: string} = {
+  const definitions: { speakUp: string; identity: string} = {
     speakUp: "Why suffer inside? Bring up that secret heating up inside you! Releave youself",
-    identity: "Worryng about Your identity? We don't knoe who you are. You're anonymous here",
-    imag: "Screenshot 2025-07-04 215927.png"
+    identity: "Worrying about Your identity? We don't know who you are. You're anonymous here",
+  }
+  const pics: {img1:string; img2:string} = {
+    img1: "Screenshot 2025-07-04 215927.png",
+    img2:  "Screenshot 2025-07-04 221505.png"
   }
 
   return (
@@ -16,16 +19,16 @@ const Landing = () => {
           <p>{definitions.speakUp}</p>
         </div>
         <div>
-          <img src={definitions.imag} alt="secret" />
+          <img src={pics.img1} alt="secret" />
         </div>
       </div>
       <div className="text-white flex mt-50 p-10 border">
         <div className="w-[50%]">
-          <h2 className="text-4xl font-black font-[helvetica] underline text-red-500">That secret you've being Hiding</h2>
-          <p>{definitions.speakUp}</p>
+          <img src={pics.img2} alt="secret" />
         </div>
         <div>
-          <img src={definitions.imag} alt="secret" />
+          <h2 className="text-4xl font-black font-[helvetica] underline text-red-500">Worrying of Exposure??</h2>
+          <p>{definitions.identity}</p>
         </div>
       </div>
     </div>
