009909a771171a7b1d1b8204639f2a3a943600d2
nianod <arnoldkk422@gmail.com>
2025-07-05 14:25:07 +0300

    landing

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,14 +1,7 @@
+import { definitions,pics } from "./LandingLogic";
+
 
 const Landing = () => {
-
-  const definitions: { speakUp: string; identity: string} = {
-    speakUp: "Why suffer inside? Bring up that secret heating up inside you! Releave youself",
-    identity: "Worrying about Your identity? We don't know who you are. You're anonymous here",
-  }
-  const pics: {img1:string; img2:string} = {
-    img1: "Screenshot 2025-07-04 215927.png",
-    img2:  "Screenshot 2025-07-04 221505.png"
-  }
 
   return (
     <div>
---
+++ src/Pages/LandingLogic.ts
@@ -0,0 +1,8 @@
+export const definitions: { speakUp: string; identity: string} = {
+    speakUp: "Why suffer inside? Bring up that secret heating up inside you! Releave youself",
+    identity: "Worrying about Your identity? We don't know who you are. You're anonymous here",
+  }
+export const pics: {img1:string; img2:string} = {
+    img1: "Screenshot 2025-07-04 215927.png",
+    img2:  "Screenshot 2025-07-04 221505.png"
+  }
