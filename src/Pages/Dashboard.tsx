b7d86e6d9b91c490ca036b329026b76c6eea8981
nianod <arnoldkk422@gmail.com>
2025-07-06 21:44:45 +0300

    finished land

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+
 
 const Dashboard = () => {
   return (
     <div>
-      <h2>Welcome</h2>
+      <div className="box p-2 rounded-2xl flex justify-center gap-4 m-auto w-fit mt-6">
+        <p className="text-xl font-bold align-center flex text-white">What is on your Mind?</p>
+        <button className=" text-white p-2 bg-red-400 rounded cursor-pointer font-bold text-xl hover:bg-red-500"
+        >
+          Reveal
+        </button>
+      </div>
     </div>
   )
 }
 
 export default Dashboard
+ 
